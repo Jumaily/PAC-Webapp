d32c50886391279ad54abc781a5f8403f2e65460
Al Jumaily <Jumaily@users.noreply.github.com>
2018-02-14 13:27:13 -0500

    Run various reports from McKesson PACS
    run modality, body parts, and studies without reports - Oracle DB

--- reports.php
+++ reports.php
@@ -1,7 +1,6 @@
 <?php
 require_once("controllers/system.cont.php");
 if(is_null($SESSION->get_var('username')) || $SESSION->get_var('username')==''){ header("location: logout.php"); exit; }
-require_once(SPATH."/classes/oracleqry.class.php"); $OrcQry = new OrcQryCls(OrcDB_USERNAME,OrcDB_PASSWORD,OrcDB_PORT,OrcDB_SERVICENAME,OrcDB_HOSTNAME);
 ?><!DOCTYPE html>
 <html lang="en">
    <head>
@@ -11,9 +10,7 @@ require_once(SPATH."/classes/oracleqry.class.php"); $OrcQry = new OrcQryCls(OrcD
 
 	    <div class="container-fluid">
           <?php $exec->get_includes("topmenu");?>
-
       
-
  		<div class="row">
 	        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
 	          <ul class="nav nav-pills">
@@ -26,55 +23,32 @@ require_once(SPATH."/classes/oracleqry.class.php"); $OrcQry = new OrcQryCls(OrcD
 	              <a href="?rqid=modalities">Modalities</a>
 	            </li>
 	          </ul>
+	          <ul class="nav nav-pills">
+	            <li <?php echo (isset($_GET["rqid"])&&$_GET["rqid"]=='reports-wo-results')?'class="active"':''; ?> >
+	              <a href="?rqid=reports-wo-results">Reports W\No Results</a>
+	            </li>
+	          </ul>	          
 	        </nav>
 
+
 	        <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
-	          <?php echo (!isset($_GET["rqid"]))?'<h3>Select Report To Run</h3>':''; ?>
-	          <div class="table-responsive">
+	        <?php echo (!isset($_GET["rqid"]))?'<h3>Select Report To Run</h3>':''; ?>
 
-	            <table class="table table-striped">
+	        <?php 
+	        # --------------- Get Body Parts & Display Report ----------------------#
+	        if(isset($_GET["rqid"])&&($_GET["rqid"]=="bodyparts")){ $exec->get_includes("reports-pacs-bodyparts"); }	 
 
-	            	<?php 
-	            		# --------------- Get Body Parts ----------------------#
-	            		if(isset($_GET["rqid"])&&($_GET["rqid"]=="bodyparts")){
-	            			$body_parts = $OrcQry->Get_Body_Parts();
-	            			?>
-	            			<thead>
-				            	<tr>
-				            		<th>Body Part ID</th>
-				            		<th>Code</th>
-				            		<th>Description</th>
-				            	</tr>
-				            </thead>
-				            <tbody>
-				            	
-				            	<?php 
-				            	for($i=0;$i<count($body_parts['MY_BODY_PART_ID']);$i++){
-				            		echo "<tr>
-				            				<td>{$body_parts['MY_BODY_PART_ID'][$i]}</td>
-				            		 		<td>{$body_parts['MY_CODE'][$i]}</td>
-				            				<td>{$body_parts['MY_DESCRIPTION'][$i]}</td>
-				            			  </tr>\n";
-				            		}
-				            	?>
-				            </tbody>
-	            	<?php 
-	            			# End of Body Parts Reports
-	            			}	 
+	        # --------------- Get Modalities Report of whats in PACS Device tabele -#
+   			elseif(isset($_GET["rqid"])&&($_GET["rqid"]=="modalities")){ $exec->get_includes("reports-pacs-modalities"); } 
+
+   			# --------------- Get Reports in PACS that has no results --------------#
+   			elseif(isset($_GET["rqid"])&&($_GET["rqid"]=="reports-wo-results")){ $exec->get_includes("reports-pacs-reports-wo-results"); }
+   			?>
 
 
-	            		# --------------- Get Current Modalities Report  ----------------------#
-	            	?>
-
-
-	            </table>
-	          </div>
 	        </main>
      	 </div>
-
-
-
       </div><!-- /.container -->
-      <?php $exec->get_includes("footer"); ?>
+      <?php $exec->get_includes("footer"); ?> 
    </body>
 </html>
