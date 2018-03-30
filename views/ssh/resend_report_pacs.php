315ff246f905a93f9ee3e4a745b6080a5d6694cb
Al Jumaily <Jumaily@users.noreply.github.com>
2018-03-30 16:55:25 -0400

    Add files via upload

--- views/ssh/resend_report_pacs.php
+++ views/ssh/resend_report_pacs.php
@@ -12,30 +12,29 @@ if(is_null($SESSION->get_var('username')) || $SESSION->get_var('username')==''){
 ?><!DOCTYPE html>
 <html lang="en">
    <body>
-		<?php
+      <?php
 
-		# Get all the reported studies without results from PACS Oracle DB
-		$swr = $OrcQry->Get_Studies_WO_Results();
-		$accs = '';
-		for($i=0;$i<count($swr['ACCESSION']);$i++){ $accs .= "{$swr['ACCESSION'][$i]}\n"; }
+      # Get all the reported studies without results from PACS Oracle DB
+      $swr = $OrcQry->Get_Studies_WO_Results();
+      $accs = '';
+      for($i=0;$i<count($swr['ACCESSION']);$i++){ $accs .= "{$swr['ACCESSION'][$i]}\n"; }
 
+      # Write all accession numbers at once to file (overwrite) & close file
+      $my_file = 'views/dl-csv/'.$filename;
+      $handle = fopen($cpath.$my_file, 'w') or die('IO file error:  '.$my_file);
+      fwrite($handle, $accs);
+      fclose($handle);
 
-		# Write them all to file (overwrite) & close file
-		$my_file = 'views/dl-csv/'.$filename;
-		$handle = fopen($cpath.$my_file, 'w') or die('IO file error:  '.$my_file);
-		fwrite($handle, $accs);
-		fclose($handle);
-
-		# link to txt file 
-		$txt_file_htmlpath = $actual_link.$my_file;
+      # link to txt file 
+      $txt_file_htmlpath = $actual_link.$my_file;
 	
-	    # issue commands and output 
-        echo "<pre>";
-        echo $sshc->issue_cmd("curl $txt_file_htmlpath > $filename");
-        echo $sshc->issue_cmd("/SMSCLN/tst/bin/resend_main 3 MPACS1 SIGNED ALL Y /home/dialup/$filename");
-        echo "</pre>";
+      # issue commands and output 
+      echo "<pre>";
+      echo $sshc->issue_cmd("curl $txt_file_htmlpath > $filename");
+      echo $sshc->issue_cmd("/SMSCLN/tst/bin/resend_main 3 MPACS1 SIGNED ALL Y /home/dialup/$filename");
+      echo "</pre>";
 
-		?>
+      ?>
    </body>
 </html>
 
@@ -55,15 +54,15 @@ class SSH_CONNECT{
       stream_set_blocking($errorStream, true);
       stream_set_blocking($stream, true);
 
-	  # Format output and display
-	  $out .= "<p>Output: ".stream_get_contents($stream)."</p>";
+      # Format output and display
+      $out .= "<p>Output: ".stream_get_contents($stream)."</p>";
       $out .= "<p><em>Error: " . stream_get_contents($errorStream)."</em></p> <hr/>";
 		
       # close the streams       
       fclose($errorStream);
-	  fclose($stream);
-	  return $out;
+      fclose($stream);
+      return $out;
       }
-
+      
    }
 ?>
