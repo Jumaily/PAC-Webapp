a4dafc4d8edd7ed638ba01397a6e8a01931904d9
Al Jumaily <Jumaily@users.noreply.github.com>
2018-04-04 14:55:47 -0400

    Update site.conf.php

--- config/site.conf.php
+++ config/site.conf.php
@@ -2,21 +2,26 @@
 # database configs  Test Server
 define('SyDB_USERNAME',"UserName");
 define('SyDB_PASSWORD',"p@ssw0rd");
-define('SyDB_DATABASE',"TST_RADDATA");
-define('SyDB_HOSTNAME',"TestServer.domain.uky.edu");
+define('SyDB_DATABASE',"TST_DB");
+define('SyDB_HOSTNAME',"testServer.domain.uky.edu");
 
 # database configs  Production Server
 define('SyDB_USERNAME_PROD',"UserName");
 define('SyDB_PASSWORD_PROD',"p@ssw0rd");
-define('SyDB_DATABASE_PROD',"PRD_RADDATA");
-define('SyDB_HOSTNAME_PROD',"ProductionServer.domain.uky.edu");
+define('SyDB_DATABASE_PROD',"PRD_DB");
+define('SyDB_HOSTNAME_PROD',"prdServer.domain.uky.edu");
 
-# Mysql DB configs
-define('MySDB_USERNAME',"UserName");
-define('MySDB_PASSWORD',"p@ssw0rd");
-define('MySDB_DATABASE',"RISuk");
-define('MySDB_HOSTNAME',"localhost");
+# Oracle DB Connection
+define('OrcDB_USERNAME',"UserName");
+define('OrcDB_PASSWORD',"p@ssw0rd");
+define('OrcDB_PORT',"1521");
+define('OrcDB_SERVICENAME',"pacs");
+define('OrcDB_HOSTNAME',"PrdOrcServer.domain.uky.edu");
 
+# RIS SSH
+define('SSH_Uname',"UserName");
+define('SSH_Pw',"p@ssw0rd");
+define('SSH_Serv','Server.domain.uky.edu');
 
 date_default_timezone_set('America/New_York');
 ?>
